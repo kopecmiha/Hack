96cca97da68ad32107c6f1cb11d800f313058530
Sergey <koliraun@ya.ru>
2019-12-13 20:59:07 +0300

    Создание проекта Vue

---
+++ dinnerfront/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <div>
+      <v-toolbar>
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-btn text>Вход</v-btn>
+          <v-btn text>Регистрация</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+
+  }),
+};
+</script>
