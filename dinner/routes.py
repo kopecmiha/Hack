6f4ca4e6fcb1751d9745a091bb6d1e4dab2df8bb
kopecmiha <58857473+kopecmiha@users.noreply.github.com>
2019-12-14 15:51:06 +0300

    Update routes.py

--- dinner/routes.py
+++ dinner/routes.py
@@ -1,7 +1,11 @@
-#from dinner import dinner
+#!/home/c/cp36696/myenv/bin/python3
+from dinner import dinner
 import os
+import time
+import base64
 import sys
 import json
+import random
 import request
 from flask import Flask, request, jsonify, render_template, flash, redirect, session, url_for, make_response
 from flask_cors import CORS
@@ -10,13 +14,13 @@ from flask_wtf import FlaskForm
 
 
 CORS(dinner)
-dinner.secret_key = 'yoг newer newer newer newer newer now this secret key'
-application = dinner
 
+application = dinner
+dinner.secret_key = 'yoг newer newer newer newer newer now this secret key'
 mysql = MySQL()
-dinner.config['MYSQL_DATABASE_USER'] = 'db_user'
-dinner.config['MYSQL_DATABASE_PASSWORD'] = 'db_pass'
-dinner.config['MYSQL_DATABASE_DB'] = 'db_user'
+dinner.config['MYSQL_DATABASE_USER'] = 'cp36696_admireso'
+dinner.config['MYSQL_DATABASE_PASSWORD'] = 'social.admire'
+dinner.config['MYSQL_DATABASE_DB'] = 'cp36696_admireso'
 dinner.config['MYSQL_DATABASE_HOST'] = 'localhost'
 dinner.config['UPLOAD_FOLDER'] = "/home/c/cp36696/dinner_near/public_html/dinner/images"
 mysql.init_app(dinner)
@@ -30,37 +34,90 @@ def hello_world():
     
 @dinner.route('/database/point_add', methods=['POST'])
 def data_load():
+	path_list = ""
 	data = request.data
 	json = eval(data)
 	title = str(json["title"])
 	description = str(json["description"])
-	images = str(json["images"])
+	images = json["images"]
+	for i in images:
+		img = base64.b64decode(i)
+		path = r"home/c/cp36696/dinner_near/public_html/dinner/images/image_point" + str(time.time()) + str(random.randint(0, 999)) + ".png"
+		with open(path, "wb") as image_file:
+			image_file.write(img)
+			path_list += (path + "::")
 	price = str(json["price"])
 	FIO = str(json["FIO"])
 	lat = str(json["lat"])
 	longi = str(json["long"])
-	cursor.execute("insert into dinner_near (title, description, images, price, FIO, latitude, longitude) values ('" + title + "','"  + description + "','" + images + "','" + price + "','" + FIO + "','" + lat + "','" + longi + "')")
+	address = str(json["address"])
+	cursor.execute("insert into dinner_near (title, description, images, price, FIO, latitude, longitude, address) values ('" + title + "','"  + description + "','" path_list "','" + price + "','" + FIO + "','" + lat + "','" + longi + "','" + address + "')")
 	conn.commit()
 	conn.close()
 	return make_response("", 200)
+
 	
 @dinner.route('/database/point_view/<ID>', methods=['GET'])
 def data_view(ID):
 	check_summ = 0
-	cursor.execute("SELECT * FROM dinner_near WHERE id = %s", (ID))
+	cursor.execute("SELECT * FROM dinner_near WHERE id IN (" + ID + ")")
 	res = cursor.fetchone()
 	if res is not None:
 		View = res
-	json = {"id" : View[0],
-		"title" : View[1],
-        "description" : View[2],
-        "images" :  View[3],
-        "price" : View[4],
-        "FIO" : View[5],
-        "lat" : View[6],
-        "long" : View[7]}
+		json = {"id" : View[0],
+			"title" : View[1],
+        	"description" : View[2],
+        	"images" :  View[3],
+        	"price" : View[4],
+        	"FIO" : View[5],
+        	"lat" : View[6],
+        	"long" : View[7],
+        	"address" : View[8]}
+	else:
+		json = None
 	return jsonify(json)
-
+	
+@dinner.route('/database/user_view/<ID>', methods=['GET'])
+def user_view(ID):
+	check_summ = 0
+	cursor.execute("SELECT * FROM dinner_users WHERE id IN (" + ID + ")")
+	res = cursor.fetchone()
+	if res is not None:
+		View = res
+		json = {"id" : View[0],
+			"email" : View[1],
+        	"create_time" : View[2],
+        	"pasport_number" :  View[3],
+        	"birthday_date" : View[4],
+        	"pasport_photo" : View[5],
+        	"avatar" : View[6],
+        	"sex" : View[7],
+        	"FIO" : View[8],
+			"miting_count" : View[9]}
+	else:
+		json = None
+	return jsonify(json)
+	
+@dinner.route('/database/point_view_all', methods=['GET'])
+def all_view():
+	cursor.execute("SELECT count(*) FROM dinner_near")
+	count = int(cursor.fetchone()[0])
+	arr = []
+	for i in range(0,count):
+		cursor.execute("SELECT * FROM dinner_near LIMIT %s, 1", (i))
+		View = cursor.fetchone()
+		json = {"id" : View[0],
+				"title" : View[1],
+        		"description" : View[2],
+        		"images" :  View[3],
+        		"price" : View[4],
+        		"FIO" : View[5],
+        		"lat" : View[6],
+        		"long" : View[7],
+        		"address" : View[8]}
+		arr.append(json)
+	return jsonify(arr)
+	
 @dinner.route('/regist', methods=['POST'])
 def regist():
 	data = request.data
@@ -71,19 +128,72 @@ def regist():
 	pasport_number = str(json["pasport_number"])
 	birthday_date = str(json["birthday_date"])
 	pasport_photo = str(json["pasport_photo"])
+	pasport_photo = base64.b64decode(pasport_photo)
 	avatar = str(json["avatar"])
+	avatar = base64.b64decode(avatar)
 	sex = str(json["sex"])
 	fio = str(json["fio"])
 	miting = str(0)
 	cursor.execute("SELECT * FROM dinner_users WHERE email = %s", (email))
 	check = cursor.fetchone()
 	if check == None:
-		cursor.execute("insert into dinner_users (email, password, create_time, pasport_number, birthday_date, pasport_photo, avatar, sex, FIO, miting_count) values ('" + email + "','"  + password + "','" + create_time + "','" + pasport_number + "','" + birthday_date + "','" + pasport_photo + "','" + avatar + "','" + sex + "','" + fio + "','" + miting + "')")
+		path = r"home/c/cp36696/dinner_near/public_html/dinner/images/image" + str(time.time()) + ".png"
+		path_av = r"home/c/cp36696/dinner_near/public_html/dinner/images/image_av" + str(time.time()) + ".png"
+		with open(path, "wb") as image_file:
+			image_file.write(pasport_photo)
+		with open(path_av, "wb") as image_file_av:
+			image_file_av.write(avatar)
+		cursor.execute("insert into dinner_users (email, password, create_time, pasport_number, birthday_date, pasport_photo, avatar, sex, FIO, miting_count) values ('" + email + "','"  + password + "','" + create_time + "','" + pasport_number + "','" + birthday_date + "','" + path + "','" + path_av + "','" + sex + "','" + fio + "','" + miting + "')")
 		conn.commit()
 		conn.close()
 		return make_response("", 200)
 	else:
-		return make_response("", 500)
+		return make_response("email error", 500)
+	
+@dinner.route('/user_update', methods=['POST'])
+def update():
+	data = request.data
+	json = eval(data)
+	ID = str(json["id"])
+	email = str(json["email"])
+	password = str(json["password"])
+	create_time = str(json["create_time"])
+	pasport_number = str(json["pasport_number"])
+	birthday_date = str(json["birthday_date"])
+	pasport_photo = str(json["pasport_photo"])
+	avatar = str(json["avatar"])
+	sex = str(json["sex"])
+	fio = str(json["fio"])
+	miting = str(json["miting_count"])
+	if email != "":
+		cursor.execute("UPDATE dinner_users SET email = %s WHERE id IN (%s)", (email, ID))
+		conn.commit()
+	if password != "":
+		cursor.execute("UPDATE dinner_users SET password = %s WHERE id IN (%s)", (password, ID))
+		conn.commit()
+	if pasport_number != "":
+		cursor.execute("UPDATE dinner_users SET pasport_number = %s WHERE id IN (%s)", (pasport_number, ID))
+		conn.commit()
+	if birthday_date != "":
+		cursor.execute("UPDATE dinner_users SET birthday_date = %s WHERE id IN (%s)", (birthday_date, ID))
+		conn.commit()
+	if pasport_photo != "":
+		cursor.execute("UPDATE dinner_users SET pasport_photo = %s WHERE id IN (%s)", (pasport_photo, ID))
+		conn.commit()
+	if avatar != "":
+		cursor.execute("UPDATE dinner_users SET avatar = %s WHERE id IN (%s)", (avatar, ID))
+		conn.commit()
+	if sex != "":
+		cursor.execute("UPDATE dinner_users SET sex = %s WHERE id IN (%s)", (sex, ID))
+		conn.commit()
+	if fio != "":
+		cursor.execute("UPDATE dinner_users SET FIO = %s WHERE id IN (%s)", (fio, ID))
+		conn.commit()
+	if miting != "":
+		cursor.execute("UPDATE dinner_users SET miting_count = %s WHERE id IN (%s)", (miting, ID))
+		conn.commit()
+	conn.close()
+	return make_response("", 200)
 	
 @dinner.route('/login', methods=['GET', 'POST'])
 def login():
@@ -97,4 +207,3 @@ def login():
 		return make_response("", 200)
 	else:
 		return make_response("", 500)
-
