f95516d7e2ebcf6be2abdd45743349ce378f63f6
kopecmiha <58857473+kopecmiha@users.noreply.github.com>
2019-12-14 08:11:19 +0300

    Update __init__.py

--- dinner/__init__.py
+++ dinner/__init__.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from config import Config
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
 
 dinner = Flask(__name__)
 dinner.config.from_object(Config)
-dinner.config['SQLALCHEMY_DATABASE_URI'] = 'mysql + pymsql://cp36696_admireso:social.admire@localhost/cp36696_admireso'
-db = SQLAlchemy(dinner)
-
-login = LoginManager(dinner)
 
 
-from dinner import routes, models
+from dinner import routes
